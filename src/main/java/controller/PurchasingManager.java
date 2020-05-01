431230eaaa5f49cebd8695302db9fe520e6c9a41
mohannashahrad <mohannashahrad2001@gmail.com>
2020-05-01 06:25:47 +0430

    perform payment

--- src/main/java/controller/PurchasingManager.java
+++ src/main/java/controller/PurchasingManager.java
@@ -1,7 +1,23 @@
 package controller;
 
-public class PurchasingManager extends Manager {
+import model.BuyLog;
+import model.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+public class PurchasingManager extends Manager{
 
     public PurchasingManager() {
+    }
+
+    // No idea what to do for creating logs!!!
+    public void performPayment(HashMap<String,String> receiverInformation , double totalPrice , double saleAmount){
+        super.person.setBalance(super.person.getBalance() - totalPrice);
+        for (Product product : super.cart.getProductsInCart().keySet()){
+            product.getSeller().setBalance(product.getSeller().getBalance() + product.getPrice());
+            storage.addSellLog(new SellLog(LocalDateTime.now(),product.getPrice(),saleAmount,((Customer)super.person)));
+            storage.addBuyLog(new BuyLog(LocalDateTime.now(),totalPrice,saleAmount,product.getSeller(),receiverInformation));
+        }
     }
 }
