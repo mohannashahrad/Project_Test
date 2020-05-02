972b6966182429c412e4b4aa82f06204ad326a75
dorsa-m <dorsa.majdi@gmail.com>
2020-05-03 00:01:16 +0430

    add Menu class

---
+++ src/main/java/View/Menu.java
@@ -0,0 +1,54 @@
+package View;
+
+import controller.Manager;
+import model.Person;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public abstract class Menu {
+    public static Scanner scanner;
+    protected static Manager manager;
+
+    public static Person getPerson() {
+        return person;
+    }
+
+    public static void setPerson(Person person) {
+        Menu.person = person;
+    }
+
+    protected static Person person;
+    protected static HashMap<String,Menu> allRootMenus;
+
+    private String name;
+    private Menu previousMenu;
+
+    static {
+        scanner = new Scanner(System.in);
+        allRootMenus = new HashMap<String, Menu>();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPreviousMenu(Menu previousMenu) {
+        this.previousMenu = previousMenu;
+    }
+
+    public Menu getPreviousMenu() {
+        return previousMenu;
+    }
+
+    public abstract void commandProcess ();
+    public abstract void show ();
+    public  void run (){
+        this.show();
+        this.commandProcess();
+    }
+}
