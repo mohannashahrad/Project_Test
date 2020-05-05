8f7011078be74fd504ad6bfe72b5257d74d53fe9
mohannashahrad <mohannashahrad2001@gmail.com>
2020-05-06 02:33:52 +0430

    cart

--- src/main/java/controller/Manager.java
+++ src/main/java/controller/Manager.java
@@ -19,6 +19,8 @@ public class Manager {
 
     public Manager() {
         storage = Storage.getStorage();
+        cart = new Cart(null);
+        storage.addCart(cart);
     }
 
     public void register (HashMap<String,String> information) throws Exception {
