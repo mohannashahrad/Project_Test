2648e26d7117e66e54b3f0b09fcd791a7972effb
dorsa-m <dorsa.majdi@gmail.com>
2020-06-11 22:06:47 +0430

    init fx

---
+++ src/main/java/graphics/Menu.java
@@ -0,0 +1,15 @@
+package graphics;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.Person;
+
+public abstract class Menu {
+    public static Stage MainStage;
+    protected static Person person;
+
+    public static Scene getScene() {
+        return null;
+    }
+
+}
