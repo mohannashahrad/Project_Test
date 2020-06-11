0e0c6d19da118c0fc9a93e4606540024acf5a9c7
dorsa-m <dorsa.majdi@gmail.com>
2020-06-11 22:48:31 +0430

    init fx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         guiStage = primaryStage;
         guiStage.setTitle("Team-18");
         guiStage.setScene();
--- src/main/java/graphics/Menu.java
+++ src/main/java/graphics/Menu.java
@@ -1,12 +1,47 @@
 package graphics;
-
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.Person;
 
-public abstract class Menu {
+import java.util.ArrayList;
+
+public class Menu {
+    private static ArrayList<Menu> allMenus = new ArrayList<>();
     public static Stage MainStage;
     protected static Person person;
+    private String name;
+    public boolean doesMenuExist(String name){
+        for (Menu menu : allMenus){
+            if (menu.getName().equals(name))
+                return true;
+        }
+        return false;
+    }
+    private Menu(String name){
+        this.setName(name);
+        allMenus.add(this);
+    }
+    public static Menu getMenuByName(String name){
+        for (Menu menu : allMenus){
+            if (menu.getName().equals(name))
+                return menu;
+        }
+        return null;
+    }
 
+    public static Person getPerson() {
+        return person;
+    }
+
+    public static void setPerson(Person person) {
+        Menu.person = person;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
