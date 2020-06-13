ff5f16268ee9115b699442d2edc2a3d419706a2b
mohannashahrad <mohannashahrad2001@gmail.com>
2020-06-13 01:16:13 -0400

    product_fxml

--- src/main/java/graphics/ProductMenu.java
+++ src/main/java/graphics/ProductMenu.java
@@ -1,6 +1,21 @@
 package graphics;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
 public class ProductMenu extends Menu {
+    @FXML
+    public Label productNameLabel;
+    @FXML
+    public ChoiceBox choiceBox;
+    @FXML
+    public ImageView imageview;
+    @FXML
+    public ListView listView;
+
 
     public void button(){
 
