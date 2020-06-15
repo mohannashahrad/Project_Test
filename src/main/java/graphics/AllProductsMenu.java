d12b2f9a95608ed3a1b057d40ac11b2adbb5aeb6
dorsa-m <dorsa.majdi@gmail.com>
2020-06-15 19:19:32 +0430

    AllProductsMenu

---
+++ src/main/java/graphics/AllProductsMenu.java
@@ -0,0 +1,7 @@
+package graphics;
+
+public class AllProductsMenu extends Menu {
+    public AllProductsMenu(Menu previousMenu) {
+        super(previousMenu, "src/main/java/graphics/fxml/AllProductsMenu.fxml");
+    }
+}
