e8df60ccbb823e35e3c915bbeae4242bb2fccda5
dorsa-m <dorsa.majdi@gmail.com>
2020-06-15 19:09:51 +0430

    run

--- src/main/java/graphics/AdminMenu.java
+++ src/main/java/graphics/AdminMenu.java
@@ -1,11 +1,5 @@
 package graphics;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
 
 public class AdminMenu extends Menu {
     public AdminMenu(Menu previousMenu) {
