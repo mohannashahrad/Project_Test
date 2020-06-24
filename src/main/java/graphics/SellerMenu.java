1e9c1ccb39bf7f90e039dd117e8d42e86d88b5c7
Bahar <asadibahar244@gmail.com>
2020-06-24 00:19:31 -0700

    Merge https://github.com/AdvProg2020/Project_team-18 into javafx
    # Conflicts:
    #	src/main/java/graphics/View.java

--- src/main/java/graphics/SellerMenu.java
+++ src/main/java/graphics/SellerMenu.java
@@ -7,15 +7,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import model.*;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
