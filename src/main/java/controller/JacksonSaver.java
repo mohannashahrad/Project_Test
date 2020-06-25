a197430475faeb3ac1caeb83b4a4f5a9a7a3a17e
dorsa-m <dorsa.majdi@gmail.com>
2020-06-25 19:46:17 +0430

    JacksonSaver

--- src/main/java/controller/JacksonSaver.java
+++ src/main/java/controller/JacksonSaver.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.type.TypeFactory;
-import model.Person;
+import model.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,11 +19,8 @@ public class JacksonSaver {
     public void dataSaver(){
         objectMapper.enableDefaultTyping();
         try {
-            writer.writeValue(new File("./dataBase/allUsers.json"),storage.getAllUsers());
-          //  writer.writeValue(new File("./dataBase/allAdmins.json"),storage.getAllAdmins());
-           // writer.writeValue(new File("./dataBase/allRequests.json"),storage.getAllRequests());
 
-           /* writer.writeValue(new File("./dataBase/allUsers.json"),storage.getAllUsers());
+            writer.writeValue(new File("./dataBase/allUsers.json"),storage.getAllUsers());
             writer.writeValue(new File("./dataBase/allSellers.json"),storage.getAllSellers());
             writer.writeValue(new File("./dataBase/allCustomers.json"),storage.getAllCustomers());
             writer.writeValue(new File("./dataBase/allAdmins.json"),storage.getAllAdmins());
@@ -36,7 +33,7 @@ public class JacksonSaver {
             writer.writeValue(new File("./dataBase/allRates.json"),storage.getAllRates());
             writer.writeValue(new File("./dataBase/allComments.json"),storage.getAllComments());
             writer.writeValue(new File("./dataBase/allSales.json"),storage.getAllSales());
-            writer.writeValue(new File("./dataBase/allRequests.json"),storage.getAllRequests());*/
+            writer.writeValue(new File("./dataBase/allRequests.json"),storage.getAllRequests());
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -44,18 +41,37 @@ public class JacksonSaver {
 
     }
     public void dataReader(){
+        reader(storage.getAllUsers(),"allUsers",Person[].class);
+        reader(storage.getAllSellers(),"allSellers", Person[].class);
+        reader(storage.getAllCustomers(),"allCustomers",Person[].class);
+        reader(storage.getAllAdmins(),"allAdmins",Person[].class);
+        reader(storage.getAllLogs(),"allLogs", Log[].class);
+        reader(storage.getAllSellLogs(),"allSellLogs",Log[].class);
+        reader(storage.getAllBuyLogs(),"allBuyLogs",Log[].class);
+        reader(storage.getAllProducts(),"allProducts", Product[].class);
+        reader(storage.getAllCategories(),"allCategories", Category[].class);
+        reader(storage.getAllDiscounts(),"allDiscounts",Discount[].class);
+        reader(storage.getAllRates(),"allRates",Rate[].class);
+        reader(storage.getAllComments(),"allComments",Comment[].class);
+        reader(storage.getAllSales(),"allSales",Sale[].class);
+        reader(storage.getAllRequests(),"allRequests",Request[].class);
+
+
+    }
+    private <T> void reader(ArrayList<T> main,String path,Class<T[]> tClass){
         try {
 
-            File file = new File("./dataBase/allUsers.json");
+            File file = new File("./dataBase/"+path+".json");
             if (!file.exists()) {
                 file.getParentFile().mkdir();
                 file.createNewFile();
                 return;
             }
-            Person[] temp = objectMapper.readValue(file, Person[].class);
-            Collections.addAll(storage.getAllUsers(),temp);
+            T[] temp = objectMapper.readValue(file, tClass);
+            Collections.addAll(main,temp);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
